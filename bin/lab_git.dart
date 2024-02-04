c30f7047e7e6bc47011a0849b27d92ee13d1c7e0
hmody360 <s441010207@st.uqu.edu.sa>
2024-02-04 14:00:24 +0300

    Feature: print name.

--- bin/lab_git.dart
+++ bin/lab_git.dart
@@ -1,5 +1,4 @@
-import 'package:lab_git/lab_git.dart' as lab_git;
 
 void main(List<String> arguments) {
-  print('Hello world: ${lab_git.calculate()}!');
+  print('Hello world: My name is mohammed!');
 }
